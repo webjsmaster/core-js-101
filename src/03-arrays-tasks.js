98e9e39bc3025b56142c92f30999847b54b5ce62
webjsmaster <web.js.master@gmail.com>
2023-10-13 14:57:33 +0300

    feat: getElementByIndexes

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -576,8 +576,11 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.length === 1 ? arr[indexes]
+    : indexes.reduce((acc, c) => (
+      Array.isArray(acc) ? acc[c] : arr[acc][c]
+    ));
 }
 
 
